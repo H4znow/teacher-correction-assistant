d373e584b2c295dd5ff45df98f0ffb2c85c0a792
H4znow <haddouamine2002@gmail.com>
2024-11-27 17:53:54 +0100

    enhance SpellChecker with max_length parameter and additional test case

--- ai_components/spell_checker.py
+++ ai_components/spell_checker.py
@@ -4,13 +4,20 @@ class SpellChecker:
     def __init__(self, model_path="./models/t5_base_spellchecker_model"):
         self.tokenizer = T5Tokenizer.from_pretrained(model_path)
         self.model = T5ForConditionalGeneration.from_pretrained(model_path)
+        self.max_length = 150
 
     def correct(self, input_text):
         # Preprocess the input text
         input_ids = self.tokenizer("spellcheck: " + input_text, return_tensors="pt").input_ids
         
         # Generate corrected text
-        outputs = self.model.generate(input_ids)
+        outputs = self.model.generate(
+            input_ids,
+            do_sample=False,
+            max_length=self.max_length,
+            # top_p=0.99,
+            num_return_sequences=1
+        )
         corrected_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
         return corrected_text
 
@@ -27,3 +34,8 @@ if __name__ == "__main__":
     # Correct the text
     corrected_text = spell_checker.correct(input_text)
     print("#" * 95, "\nCorrected text:", corrected_text)
+
+    input_text2 = "the schol is vry far awy from my yhouse"
+
+    corrected_text2 = spell_checker.correct(input_text2)
+    print("#" * 95, "\nCorrected text:", corrected_text2)
