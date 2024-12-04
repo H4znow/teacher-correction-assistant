4d5ecd9e4419303313acdeb045693143697771fc
H4znow <haddouamine2002@gmail.com>
2024-12-04 10:11:19 +0100

    feat(grammar_models) : dowloaded and test new grammar model 'grammar_synthesis_small' refractor(grammar): formatted grammar checker class output to enhance models

--- ai_components/grammar_checker2.py
+++ ai_components/grammar_checker2.py
@@ -1,22 +1,21 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
-import torch
 
 class GrammarChecker:
-    def __init__(self, model_path="./models/prithivida_grammar_error_correcter_v1"):
+    def __init__(self, model_path="./models/grammar-synthesis-small"):
         # Load the locally saved model and tokenizer
         self.tokenizer = T5Tokenizer.from_pretrained(model_path)
         self.model = T5ForConditionalGeneration.from_pretrained(model_path)
 
     def correct(self, text):
         # Preprocess the input text
-        input_text = "gec: " + text  # "gec:" is the prefix for grammar correction in this model
-        input_ids = self.tokenizer.encode(input_text, return_tensors="pt")
+        input_text = text  # Prefix for grammar correction in this model
+        input_ids = self.tokenizer.encode(input_text, return_tensors="pt", truncation=True)
 
         # Generate corrected text
         outputs = self.model.generate(input_ids, max_length=512)
         corrected_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
         return corrected_text
-
+    
     def summary(self):
         # Calculate and print the model's summary
         total_params = sum(p.numel() for p in self.model.parameters())
