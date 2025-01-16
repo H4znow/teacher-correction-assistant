ba7f708ad152e107cb9930904d9ca5f3873168eb
Mariia Berdnyk <80634357+BadUkrainianWolf@users.noreply.github.com>
2025-01-16 14:45:04 +0100

    adding remove background and crop functionality

--- front/server.py
+++ front/server.py
@@ -1,6 +1,9 @@
 import torch
 from flask import Flask, request, render_template, jsonify
 import os
+
+from lib.preprocess_image import remove_background, crop_lines
+
 # import torch_xla.core.xla_model as xm
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -8,19 +11,22 @@ os.chdir(ROOT_DIR)
 app = Flask(__name__)
 device = torch.device("cpu")
 
-def preprocess_image(image_path):
+def preprocess_image(image_path, image_name):
     # Placeholder: Preprocess the image and return the preprocessed image path and list of image paths for lines
     processed_image = "static/images/processed/preprocessed_image.png"
 
     # Directory where line images are stored
     line_images_dir = "static/images/processed/lines"
-    os.makedirs(line_images_dir, exist_ok=True)
+    # os.makedirs(line_images_dir, exist_ok=True)
+
+    image = remove_background(image_path, image_name, "../front/static/images/processed", "preprocessed_image.png")
+
+    crop_lines(image, "../front/static/images/processed/lines")
 
     # Example: Assuming images are already created or you process them here
     # Get all image files from the directory
-    lines_images = [os.path.join(line_images_dir, filename) for filename in os.listdir(os.path.join(ROOT_DIR, line_images_dir)) if
+    lines_images = [os.path.join(line_images_dir, filename) for filename in os.listdir("../front/static/images/processed/lines") if
                     filename.endswith('.png')]
-    print(lines_images)
 
     return processed_image, lines_images
 
@@ -48,8 +54,8 @@ def check_image_quality():
     # Handle user decision on image quality
     decision = request.json.get('decision')
     if decision == 'yes':
-        image_path = "static/images/uploaded_image.jpg"  # Example image path
-        preprocessed_image, lines = preprocess_image(image_path)
+        image_path = "../front/static/images"  # Example image path
+        preprocessed_image, lines = preprocess_image(image_path, "uploaded_image.jpg")
         # {{url_for('static', preprocessed_image)}}
         return jsonify({"status": "preprocessed", "image": preprocessed_image, "lines": lines})
     return jsonify({"status": "redo"})
